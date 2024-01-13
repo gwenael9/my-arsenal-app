fdcfbf9c8a2a8a9f187f67c51ac82ad21de4f3c4
gwenael9 <gwenaelgueho@gmail.com>
2024-01-13 13:50:58 +0100

    structure front

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    webpackDevMiddleware: config => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        }
+        return config
+    },
+  }
