1e0f4e1742df30fdd246931049ce4ab3069406b3
gwenael9 <gwenaelgueho@gmail.com>
2024-04-28 16:10:36 +0200

    seeDB

--- backend/src/lib/seedDb.ts
+++ backend/src/lib/seedDb.ts
@@ -7,10 +7,13 @@ dotenv.config();
 async function seedDb() {
     const userService = new UserService();
 
-    console.log(process.env.email);
+    console.info("toto");
+    console.info("test :", process.env.email);
 
     // vérifie si un utilisateur avec l'e-mail spécifié existe déjà
     const user = await userService.findUserByEmail(process.env.email || "");
+
+    console.info("user :", user);
 
   if (!user) {
     const newUser = await userService.createUser({
