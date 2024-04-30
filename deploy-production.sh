a0f21b141926ed24f15b15aa50cc2cfa5130f0df
gwenael9 <gwenaelgueho@gmail.com>
2024-04-30 22:41:19 +0200

    test webhook

--- deploy-production.sh
+++ deploy-production.sh
@@ -3,7 +3,7 @@
 git fetch origin && git reset --hard origin/main && git clean -f -d && \
 # on stop tout les containers dans le compose de prod
 docker compose -f docker-compose.prod.yml down && \
-# on pull la der niere version des images dans le compose de prod
+# on pull la derniere version des images dans le compose de prod
 docker compose -f docker-compose.prod.yml pull && \
 # on relance le compose en précisant un fichier env qui contient toutes les variables nécessaires pour l'ensemble des services
 docker compose -f docker-compose.prod.yml --env-file .env.production up -d;
