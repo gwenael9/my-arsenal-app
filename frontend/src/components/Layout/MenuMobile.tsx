8a68fa829dc2a7dade7dcf52f83e3924c3b86593
gwenael9 <gwenaelgueho@gmail.com>
2024-06-08 10:57:14 +0200

    mobile

--- frontend/src/components/Layout/MenuMobile.tsx
+++ frontend/src/components/Layout/MenuMobile.tsx
@@ -3,36 +3,50 @@ import { Button } from "../ui/button";
 import { X } from "lucide-react";
 
 interface NavLink {
-    name: string;
-    link: string;
+  name: string;
+  link: string;
 }
 
 interface MenuMobileProps {
-    isOpen: boolean;
-    closeMenu: () => void;
-    navLink: NavLink[];
+  isOpen: boolean;
+  closeMenu: () => void;
+  navLink: NavLink[];
 }
 
-export default function MenuMobile({ isOpen, closeMenu, navLink }: MenuMobileProps) {
-    return (
-        <div
-        className={`fixed top-10 left-0 right-0 mx-auto z-50 transition-transform duration-300 ease-in-out max-w-[500px] bg-white p-5 rounded ${
-          isOpen ? "transform translate-x-0" : "transform -translate-x-full -left-full"
-        }`}
-        style={{ width: 'calc(100% - 40px)', height: 'calc(100% - 100px)' }}
-        >
-        <div className={`flex flex-col gap-10`}>
+export default function MenuMobile({
+  isOpen,
+  closeMenu,
+  navLink,
+}: MenuMobileProps) {
+  return (
+    <div
+      className={`fixed w-full top-10 z-50 transition-transform duration-300 ease-in-out ${
+        isOpen ? "transform translate-x-0" : "transform -translate-x-full"
+      }`}
+    >
+      <div
+        className="bg-white p-5 rounded mx-10"
+        style={{ height: "calc(100vh - 80px)" }}
+      >
+        <div className="flex flex-col gap-10 h-full relative">
           <div className="flex justify-center flex-col items-center gap-2">
             {navLink.map((n, index) => (
-              <Link key={index} href={n.link} className="text-xl uppercase font-bold">
+              <Link
+                key={index}
+                href={n.link}
+                className="text-xl uppercase font-bold"
+              >
                 {n.name}
               </Link>
             ))}
           </div>
           <div className="absolute bottom-0 right-0 p-4">
-            <Button variant={"arrowCard"} onClick={closeMenu}><X /></Button>
+            <Button variant={"arrowCard"} onClick={closeMenu}>
+              <X />
+            </Button>
           </div>
         </div>
       </div>
-    )
+    </div>
+  );
 }
